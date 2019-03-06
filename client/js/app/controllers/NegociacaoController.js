d2d31e9f989dc650e04b0e01b83a58a32e23d571
unknown <limacosta25@hotmail.com>
2019-03-06 17:35:45 -0300

    lapidando o Negociações controller e os serviços

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,81 +6,70 @@ class NegociacaoController {
     this._inputQuantidade = $('#quantidade');
     this._inputValor = $('#valor');
 
+    this._negociacaoService = new NegociacaoService();
     this._listaNegociacoes = new Bind(
       new ListaNegociacoes(),
       new NegociacoesView($('#negociacoesView')),
       'adiciona', 'esvazia', 'ordena', 'inverteOrdem'
     );
-
     this._mensagem = new Bind(
       new Mensagem(),
       new MensagemView($('#mensagemView')),
       'texto'
     );
 
-	  ConnectionFactory
-	  	.getConnection()
-	  	.then(connection => new NegociacaoDao(connection))
-	  	.then(dao => dao.listaTodos())
-	  	.then(negociacoes =>
-	  		negociacoes.forEach(negociacao =>
-	  			this._listaNegociacoes.adiciona(negociacao))
-	  	).catch(erro => this._mensagem.texto = erro);
+    this._init();
+  }
 
+  _init() {
+    this._negociacaoService
+        .lista()
+        .then(negociacoes =>
+          negociacoes.forEach(negociacao =>
+  	  			this._listaNegociacoes.adiciona(negociacao)))
+        .catch(erro => this._mensagem.texto = erro);
+
+      setInterval(() => this.importaNegociacoes(), 3000);
+  }
+
+  isEquals(negociacao) {
+    return JSON.stringify(this) == JSON.stringify(negociacao)
   }
 
   adiciona(event) {
     event.preventDefault();
 
-	  ConnectionFactory
-	  	.getConnection()
-	  	.then(connection => {
-			let negociacao = this._criaNegociacao();
-
-			new NegociacaoDao(connection)
-				.adiciona(negociacao)
-				.then(() => {
-					this._listaNegociacoes.adiciona(negociacao);
-    					this._mensagem.texto = "Negociação adicionada com sucesso";
-    					this._limpaFormulario();
-				})
-		})
-	  	.catch(erro => this._mensagem.texto = erro);
+    let negociacao = this._criaNegociacao();
+    this._negociacaoService
+        .cadastrar(negociacao)
+        .then(mensagem => {
+          this._listaNegociacoes.adiciona(negociacao);
+          this._mensagem.texto = mensagem;
+          this._limpaFormulario();
+        })
+        .catch(erro => this._mensagem.texto = erro);
   }
 
   importaNegociacoes() {
-    let service = new NegociacaoService();
-
-    Promise.all([
-      service.obterNegociacoesDaSemana(),
-      service.obterNegociacoesDaSemanaAnterior(),
-      service.obterNegociacoesDaSemanaRetrasada()
-    ])
-    .then(negociacoes => {
-      negociacoes = negociacoes.reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-
-      return negociacoes.filter(negociacao =>
-          !this._listaNegociacoes.negociacoes.some(n =>
-              JSON.stringify(negociacao) == JSON.stringify(n)))
-    })
-    .then(negociacoes => {
-      negociacoes
-        .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
-        .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
-        this._mensagem.texto = 'Negociações importadas com sucesso';
-    })
-    .catch(error => this._mensagem.texto = error);
+    this._negociacaoService
+        .importa(this._listaNegociacoes.negociacoes)
+        .then(negociacoes => {
+          negociacoes
+            .reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+            .forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociações importadas com sucesso';
+        })
+        .catch(error => this._mensagem.texto = error);
   }
 
   apaga() {
-	  ConnectionFactory
-	  	.getConnection()
-	  	.then(connection => new NegociacaoDao(connection))
-	  	.then(dao => dao.apagaTodos())
-	  	.then(msg => {
-			 this._listaNegociacoes.esvazia();
-    			this._mensagem.texto = msg;
-		}).catch(error => this._mensagem.texto = error);
+    this._negociacaoService
+        .apaga()
+        .then(msg => {
+          this._listaNegociacoes.esvazia();
+       		this._mensagem.texto = msg;
+        })
+        .catch(erro => this._mensagem.texto = erro);
   }
 
   ordenar(coluna) {
--- client/js/app/services/NegociacaoService.js
+++ client/js/app/services/NegociacaoService.js
@@ -3,6 +3,59 @@ class NegociacaoService {
     this._http = new HttpService();
   }
 
+  cadastrar(negociacao) {
+    return ConnectionFactory
+            .getConnection()
+            .then(connection => new NegociacaoDao(connection))
+            .then(dao => dao.adiciona(negociacao))
+            .then(() => "Negociação adicionada com sucesso")
+            .catch(erro => {
+              console.log(erro);
+              throw new Error("Não foi possível adicionar a negociação")
+            })
+  }
+
+  lista() {
+    return ConnectionFactory
+      	  	.getConnection()
+      	  	.then(connection => new NegociacaoDao(connection))
+      	  	.then(dao => dao.listaTodos())
+            .catch(erro => {
+              console.log(erro);
+              throw new Error("Não foi possível obter as negociações")
+            });
+  }
+
+  apaga() {
+    return ConnectionFactory
+      	  	.getConnection()
+      	  	.then(connection => new NegociacaoDao(connection))
+      	  	.then(dao => dao.apagaTodos())
+            .then(() => "Negociação removidas com sucesso")
+            .catch(erro => {
+              console.log(erro);
+              throw new Error("Não foi possível apagar as negociações")
+            });
+  }
+
+  importa(listaAtual) {
+    return Promise.all([
+      this.obterNegociacoesDaSemana(),
+      this.obterNegociacoesDaSemanaAnterior(),
+      this.obterNegociacoesDaSemanaRetrasada()
+    ])
+    .then(negociacoes => {
+      negociacoes = negociacoes.reduce((arrayAchatado, array) => arrayAchatado.concat(array), [])
+      return negociacoes.filter(negociacao =>
+          !listaAtual.some(n =>
+               JSON.stringify(negociacao) == JSON.stringify(n)))
+    })
+    .catch(erro => {
+      console.log(erro);
+      throw new Error("Não foi possível apagar as negociações")
+    });
+  }
+
   obterNegociacoesDaSemana() {
     return this._http
       .get('negociacoes/semana')
